502e5ff0a6344aec39edc38db0bff576868692ca
lewis ushindi <frenziecodes@gmail.com>
2023-11-18 15:47:34 +0300

    initial commit

--- includes/class-zestcompression.php
+++ includes/class-zestcompression.php
@@ -7,7 +7,7 @@ class ZestCompressionPlugin {
     }
 
     public function init() {
-        add_filter('wp_handle_upload', array($this, 'handle_upload'));
+        add_filter('wp_generate_attachment_metadata', array($this, 'compress_uploaded_image'), 10, 2);
         add_action('admin_menu', array($this, 'add_plugin_page'));
         add_action('admin_init', array($this, 'page_init'));
     }
@@ -69,16 +69,10 @@ class ZestCompressionPlugin {
     }
 
     public function sanitize($input) {
-        // Ensure $input is an array to prevent potential errors
         $input = is_array($input) ? $input : array();
 
-        // Sanitize and validate input here
         $new_input = array();
-
-        // Example: Sanitize the 'enable_compression' option
         $new_input['enable_compression'] = isset($input['enable_compression']) ? 1 : 0;
-
-        // Example: Sanitize the 'compression_quality' option
         $new_input['compression_quality'] = isset($input['compression_quality']) ? intval($input['compression_quality']) : '';
 
         return $new_input;
@@ -99,73 +93,37 @@ class ZestCompressionPlugin {
         echo 'Configure compression settings below:';
     }
 
-    
-
-    private function compress_image($file_path, $mime_type) {
+    public function compress_uploaded_image($metadata, $attachment_id) {
         $settings = get_option('zest_compression_settings');
 
-        if (!empty($settings['enable_compression'])) {
-            if (strpos($mime_type, 'image/jpeg') === 0) {
-                $this->compress_jpeg($file_path, $settings['compression_quality']);
-            } elseif (strpos($mime_type, 'image/png') === 0) {
-                $this->compress_png($file_path, $settings['compression_quality']);
-            } elseif (strpos($mime_type, 'image/gif') === 0) {
-                $this->compress_gif($file_path);
+        if (!empty($settings['enable_compression']) && isset($metadata['file'])) {
+            $file_path = get_attached_file($attachment_id);
+            
+            // Check if the file exists
+            if (file_exists($file_path)) {
+                $this->compress_image($file_path, $settings['compression_quality']);
+            } else {
+                // Handle the case where the file doesn't exist
+                error_log('File does not exist: ' . $file_path);
             }
         }
+
+        return $metadata;
     }
 
-    private function compress_jpeg($file_path, $quality) {
-        // Load the image
-        $image = imagecreatefromjpeg($file_path);
-    
-        // Create a new JPEG file
-        imagejpeg($image, $file_path, $quality);
-    
-        // Free up memory
-        imagedestroy($image);
-    }
-    
-    private function compress_png($file_path, $compression_level) {
-        // Load the image
-        $image = imagecreatefrompng($file_path);
-    
-        // Create a new PNG file
-        imagepng($image, $file_path, $compression_level);
-    
-        // Free up memory
-        imagedestroy($image);
-    }
-    
-    private function compress_gif($file_path) {
-        // Load the image
-        $image = imagecreatefromgif($file_path);
-    
-        // Create a new GIF file
-        imagegif($image, $file_path);
-    
-        // Free up memory
-        imagedestroy($image);
-    }
-    
 
-    public function handle_upload($file) {
-        if (strpos($file['type'], 'image') === 0) {
-            $this->compress_image($file['file'], $file['type']);
-            add_action('admin_notices', array($this, 'compression_success_notice'));
+    private function compress_image($file_path, $quality) {
+        if (false === function_exists('wp_get_image_editor')) {
+            include ABSPATH . 'wp-admin/includes/image.php';
         }
 
-        return $file;
-    }
+        $image_editor = wp_get_image_editor($file_path);
 
-    public function compression_success_notice() {
-        ?>
-        <div class="notice notice-success is-dismissible">
-            <p><?php _e('Image compression is successful!', 'zest-compression'); ?></p>
-        </div>
-        <?php
+        if (!is_wp_error($image_editor)) {
+            $image_editor->set_quality($quality);
+            $image_editor->save($file_path);
+        }
     }
-
 }
 
 // Instantiate the class
