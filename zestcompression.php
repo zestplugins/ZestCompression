698d00ce23a3eb3b1f3a16dde5200c2f65583c56
Lewis Ushindi <coderflame3@gmail.com>
2023-11-18 11:55:57 +0300

    Create zestcompression.php

---
+++ zestcompression.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Zest Compression
+ * Description: Lossless image compression for JPEG, PNG, and GIF.
+ * Version: 1.0
+ * Author: zestplugins
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
